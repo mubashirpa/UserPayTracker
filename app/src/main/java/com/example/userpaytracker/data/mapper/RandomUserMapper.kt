4182ab22124d85423f7fd0cf126a462906d68005
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-11 14:08:33 +0530

    feat: Add RandomUserMapper and User model
    - Introduce `RandomUserMapper` to transform `RandomUsersDto` into a list of `User` objects.
    - Add a `User` data class to represent user data.

---
+++ app/src/main/java/com/example/userpaytracker/data/mapper/RandomUserMapper.kt
@@ -0,0 +1,11 @@
+package com.example.userpaytracker.data.mapper
+
+import com.example.userpaytracker.data.remote.dto.RandomUsersDto
+import com.example.userpaytracker.domain.model.User
+
+fun RandomUsersDto.toUser(): List<User> =
+    results.map {
+        User(
+            name = "${it.name.first} ${it.name.last}",
+        )
+    }
---
+++ app/src/main/java/com/example/userpaytracker/domain/model/User.kt
@@ -0,0 +1,5 @@
+package com.example.userpaytracker.domain.model
+
+data class User(
+    val name: String? = null,
+)
