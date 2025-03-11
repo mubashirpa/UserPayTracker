b17df79dbc17b7088dd93c51909dc9b0a4587b0a
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-11 14:10:30 +0530

    feat: Add RandomUserService interface for fetching random users

---
+++ app/src/main/java/com/example/userpaytracker/data/remote/api/RandomUserService.kt
@@ -0,0 +1,13 @@
+package com.example.userpaytracker.data.remote.api
+
+import com.example.userpaytracker.data.remote.dto.RandomUsersDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RandomUserService {
+    @GET
+    suspend fun getRandomUsers(
+        @Query("page") page: Int = 1,
+        @Query("results") results: Int = 20,
+    ): RandomUsersDto
+}
