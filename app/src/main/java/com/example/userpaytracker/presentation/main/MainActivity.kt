ec7997a4e2030eb5f9ef08cf3cf984a1c126a0a7
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-11 14:15:07 +0530

    Refactor: Move MainActivity and update references
    - Move `MainActivity` from the root package to the `presentation.main` package.
    - Introduce view binding in `MainActivity`.
    - Set up `DynamicColors` for `MainActivity`.
    - Add `NavHostFragment` to `activity_main.xml` and configure the navigation controller.

--- app/src/main/java/com/example/userpaytracker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/userpaytracker/presentation/main/MainActivity.kt
@@ -1,20 +1,32 @@
-package com.example.userpaytracker
+package com.example.userpaytracker.presentation.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.userpaytracker.databinding.ActivityMainBinding
+import com.example.userpaytracker.navigation.findNavController
+import com.google.android.material.color.DynamicColors
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        DynamicColors.applyToActivityIfAvailable(this)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findNavController(binding.navHostFragment.id)
     }
 }
