ff6a38dca374ee15ef1b789171f3ea38a0bc69af
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-12 12:56:58 +0530

    feat: Implement data-to-domain mapping for User data

--- app/src/main/java/com/example/userpaytracker/data/mapper/RandomUserMapper.kt
+++ app/src/main/java/com/example/userpaytracker/data/mapper/RandomUserMapper.kt
@@ -1,13 +1,27 @@
 package com.example.userpaytracker.data.mapper
 
+import com.example.userpaytracker.data.local.entity.UserEntity
 import com.example.userpaytracker.data.remote.dto.RandomUsersDto
 import com.example.userpaytracker.domain.model.User
 
-fun RandomUsersDto.toUser(): List<User> =
+fun RandomUsersDto.toUserEntity(): List<UserEntity> =
     results
         ?.map {
-            User(
+            UserEntity(
                 name = "${it.name?.first} ${it.name?.last}",
+                paymentAmount = 2500.0,
+                paymentCompleted = false,
+                paymentMethod = null,
                 picture = it.picture?.medium,
             )
         }.orEmpty()
+
+fun UserEntity.toUser(): User =
+    User(
+        id = id,
+        name = name,
+        paymentAmount = paymentAmount,
+        paymentCompleted = paymentCompleted,
+        paymentMethod = paymentMethod,
+        picture = picture,
+    )
