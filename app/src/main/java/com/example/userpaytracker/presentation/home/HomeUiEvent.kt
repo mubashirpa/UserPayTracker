b7059cdb88e78319016cb437e20c43734eb056c1
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-12 15:27:49 +0530

    feat: Implement AddVisitor and ClearUsers actions in HomeViewModel

--- app/src/main/java/com/example/userpaytracker/presentation/home/HomeUiEvent.kt
+++ app/src/main/java/com/example/userpaytracker/presentation/home/HomeUiEvent.kt
@@ -1,5 +1,11 @@
 package com.example.userpaytracker.presentation.home
 
 sealed class HomeUiEvent {
+    data class AddVisitor(
+        val name: String,
+    ) : HomeUiEvent()
+
+    object ClearUsers : HomeUiEvent()
+
     object Retry : HomeUiEvent()
 }
--- app/src/main/java/com/example/userpaytracker/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/userpaytracker/presentation/home/HomeViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.userpaytracker.core.Result
 import com.example.userpaytracker.domain.model.User
+import com.example.userpaytracker.domain.usecase.AddVisitorUseCase
+import com.example.userpaytracker.domain.usecase.ClearUsersUseCase
 import com.example.userpaytracker.domain.usecase.GetRandomUsersUseCase
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -13,6 +15,8 @@ import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val getRandomUsersUseCase: GetRandomUsersUseCase,
+    private val addVisitorUseCase: AddVisitorUseCase,
+    private val clearUsersUseCase: ClearUsersUseCase,
 ) : ViewModel() {
     private val _usersResult: MutableLiveData<Result<List<User>>> = MutableLiveData(Result.Empty())
     val usersResult: LiveData<Result<List<User>>> = _usersResult
@@ -23,6 +27,10 @@ class HomeViewModel(
 
     fun onEvent(event: HomeUiEvent) {
         when (event) {
+            is HomeUiEvent.AddVisitor -> addVisitor(event.name)
+
+            HomeUiEvent.ClearUsers -> clearUsers()
+
             HomeUiEvent.Retry -> getRandomUsers()
         }
     }
@@ -34,5 +42,18 @@ class HomeViewModel(
                     _usersResult.value = result
                 }.launchIn(this)
         }
+    }
+
+    private fun addVisitor(name: String) {
+        addVisitorUseCase(name).launchIn(viewModelScope)
+    }
+
+    private fun clearUsers() {
+        clearUsersUseCase()
+            .onEach { result ->
+                if (result is Result.Success) {
+                    getRandomUsers()
+                }
+            }.launchIn(viewModelScope)
     }
 }
