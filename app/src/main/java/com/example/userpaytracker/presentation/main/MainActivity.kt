243f460a88da638aabda8d7140047d13a7ce2895
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-12 11:06:09 +0530

    refactor: Remove the previous implementation using `setOnApplyWindowInsetsListener`

--- app/src/main/java/com/example/userpaytracker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/userpaytracker/presentation/main/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.userpaytracker.presentation.main
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.userpaytracker.databinding.ActivityMainBinding
 import com.example.userpaytracker.navigation.findNavController
 import com.google.android.material.color.DynamicColors
@@ -20,12 +18,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         findNavController(binding.navHostFragment.id)
     }
