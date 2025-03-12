c2c0a9e1995be782cbbd1db1d98667440c4ad52f
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-12 11:07:43 +0530

    feat: Implement HomeViewModel and HomeUiEvent
    - Create `HomeUiEvent` sealed class to handle UI events like `Retry`.
    - Implement `HomeViewModel` to manage the state and logic for the home screen.
    - Add `getRandomUsers()` function to `HomeViewModel` to fetch random users using `GetRandomUsersUseCase`.
    - Introduce `onEvent()` function in `HomeViewModel` to process `HomeUiEvent`s.
    - Update `HomeViewModel` to use `LiveData` to expose the result of user fetching.

---
+++ app/src/main/java/com/example/userpaytracker/presentation/home/HomeUiEvent.kt
@@ -0,0 +1,5 @@
+package com.example.userpaytracker.presentation.home
+
+sealed class HomeUiEvent {
+    object Retry : HomeUiEvent()
+}
---
+++ app/src/main/java/com/example/userpaytracker/presentation/home/HomeViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.userpaytracker.presentation.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.userpaytracker.core.Result
+import com.example.userpaytracker.domain.model.User
+import com.example.userpaytracker.domain.usecase.GetRandomUsersUseCase
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class HomeViewModel(
+    private val getRandomUsersUseCase: GetRandomUsersUseCase,
+) : ViewModel() {
+    private val _usersResult: MutableLiveData<Result<List<User>>> = MutableLiveData(Result.Empty())
+    val usersResult: LiveData<Result<List<User>>> = _usersResult
+
+    init {
+        getRandomUsers()
+    }
+
+    fun onEvent(event: HomeUiEvent) {
+        when (event) {
+            HomeUiEvent.Retry -> getRandomUsers()
+        }
+    }
+
+    private fun getRandomUsers() {
+        getRandomUsersUseCase()
+            .onEach { result ->
+                _usersResult.value = result
+            }.launchIn(viewModelScope)
+    }
+}
