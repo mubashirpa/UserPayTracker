ba2e0623be83e3f00e8d32cd599f7bd71efd0e35
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-14 12:21:14 +0530

    feat: Update GetRandomUsersUseCase to handle local data and network requests

--- app/src/main/java/com/example/userpaytracker/domain/usecase/GetRandomUsersUseCase.kt
+++ app/src/main/java/com/example/userpaytracker/domain/usecase/GetRandomUsersUseCase.kt
@@ -1,22 +1,40 @@
 package com.example.userpaytracker.domain.usecase
 
+import com.example.userpaytracker.R
 import com.example.userpaytracker.core.Result
+import com.example.userpaytracker.core.UiText
 import com.example.userpaytracker.domain.model.User
 import com.example.userpaytracker.domain.repository.RandomUserRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class GetRandomUsersUseCase(
     private val repository: RandomUserRepository,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) {
-    suspend operator fun invoke(
+    operator fun invoke(
         page: Int = 1,
         results: Int = 20,
     ): Flow<Result<List<User>>> =
-        withContext(ioDispatcher) {
-            repository.getRandomUsers(page, results)
-        }
+        flow {
+            try {
+                emit(Result.Loading())
+                repository.getRandomUsersFromLocal().collect { users ->
+                    if (users.isNotEmpty()) {
+                        emit(Result.Success(users))
+                    } else {
+                        repository.getRandomUsersToLocal(page, results)
+                    }
+                }
+            } catch (e: Exception) {
+                val message =
+                    e.message?.let {
+                        UiText.DynamicString(it)
+                    } ?: UiText.StringResource(R.string.error_unknown)
+                emit(Result.Error(message))
+            }
+        }.flowOn(ioDispatcher)
 }
